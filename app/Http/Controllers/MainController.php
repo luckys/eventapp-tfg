b8a5792768dfc2ebdce97afade97b3154b458343
Luckys <cfour@openmailbox.org>
2016-09-09 10:45:39 +0100

    Return to master

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,10 +3,17 @@
 namespace EventApp\Http\Controllers;
 
 
+use EventApp\Domain\Models\Event;
+use EventApp\Domain\Models\Talk;
+
 class MainController extends Controller
 {
     public function index()
     {
-        return view('front.event.view-all');
+        $events = Event::take(3)->get();
+
+        $talks = Talk::take(3)->get();
+
+        return view('front.main', compact('events', 'talks'));
     }
 }
