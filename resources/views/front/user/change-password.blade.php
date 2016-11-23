250115a022019da5f9810d7fff87a9e56d1f0d6b
Luckys <cfour@openmailbox.org>
2016-11-23 20:51:37 +0000

    Fixed bug

--- resources/views/front/user/change-password.blade.php
+++ resources/views/front/user/change-password.blade.php
@@ -9,7 +9,7 @@
                     <div class="col-md-4 col-md-offset-4">
                         @include('admin.partials.message')
                         <div class="panel panel-default">
-                            <div class="panel-heading">Formulario para el envio de enlace de cambio de contraseña</div>
+                            <div class="panel-heading">Formulario para resetear la contraseña</div>
                             <div class="panel-body" style="height: auto">
                                 @include('admin.errors.form_error')
 
